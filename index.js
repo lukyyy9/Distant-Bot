c1c967cff82fd19137641bbb4075fba8515ef209
Lukyyy9 <lucas.buonocore@outlook.fr>
2024-03-05 09:55:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
 
     if (interaction.data.name == 'share') {
       let url = interaction.data.options[0].value
-      let match = url.match(/instagram.com\/*\/([^\/]+)/);
+      let match = url.match(/instagram.com\/([a-zA-Z])\/([^\/]+)/);
       if (!match) {
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
