cc0c04f6c2e80fd88b0502bf2c1c3f9ba50c00b0
grey <leminemahjoub@gmail.com>
2024-03-06 12:35:02 +0100

    Test with DD

--- index.js
+++ index.js
@@ -27,41 +27,27 @@ app.post('/interactions', verifyMiddleware, async (req, res) => {
                         data: { content: `Pong ${member.user.username}! 🏓` },
                     });
 
-					case 'share':
-						const url = data.options[0].value;
-						// Match URLs starting with the specified pattern for Instagram Reels on ddinstagram
-						const match = url.match(/^https:\/\/www\.ddinstagram\.com\/reel\/([a-zA-Z0-9_\-]+)\/\?igshid=[a-zA-Z0-9_\-]+$/);
+                case 'share':
+                    const url = data.options[0].value;
+                    const match = url.match(/^https:\/\/www\.instagram\.com\/reel\/([a-zA-Z0-9_\-]+)\/\?igshid=[a-zA-Z0-9_\-]+$/);
 
-						if (match) {
-							// Assume transformUrl function can handle URLs from ddinstagram to fetch a direct video link
-							try {
-								const directVideoUrl = await transformUrl(url);
-								if (!directVideoUrl) {
-									return res.send({
-										type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-										data: { content: 'Failed to obtain a direct video URL.' },
-									});
-								}
+                    if (match) {
+                        const reelId = match[1];
+                        const directVideoUrl = `https://www.ddinstagram.com/reel/${reelId}/?igsh=MTVjazh1cm04czhmbw==`;
 
-								return res.send({
-									type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-									data: { content: `Here is the direct link to the Instagram video: ${directVideoUrl}\n🛠️ Streaming through the desktop client is not supported yet. 🛠️` },
-								});
-							} catch (error) {
-								console.error('Error processing Instagram reel:', error);
-								return res.status(500).send({ content: 'An error occurred while processing your request.' });
-							}
-						} else {
-							return res.send({
-								type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-								data: { content: 'Invalid Instagram reel link provided.' },
-							});
-					}
+                        return res.send({
+                            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                            data: { content: `Here is the direct link to the Instagram video: ${directVideoUrl}\n🛠️ Streaming through the desktop client is not supported yet. 🛠️` },
+                        });
+                    } else {
+                        return res.send({
+                            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                            data: { content: 'Invalid Instagram reel link provided.' },
+                        });
+                    }
+                    break;
             }
             break;
-
-        default:
-            console.log('Unhandled interaction type:', type);
     }
 });
 
