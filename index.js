79ee60351bb193afe5c3beee3e432c12ba544957
grey <leminemahjoub@gmail.com>
2024-03-06 13:51:54 +0100

    Fix

--- index.js
+++ index.js
@@ -40,28 +40,28 @@ app.post('/interactions', verifyMiddleware, async (req, res) => {
                     });
 
                 case 'share':
-                    let url = data.options[0].value;
+                    let url = new URL(data.options[0].value).hostname.split('.')[0];
                     let videoType = '';
-                    switch (url.split('.')[1]+'.') {
-                        case platform.Instagram:
-                            url = url.replace(platform.Instagram, altPlatform.Instagram);
-                            videoType = 'Reel';
-                            break;
-                        case platform.TikTok:
-                            url = url.replace(platform.TikTok, altPlatform.TikTok);
-                            videoType = 'TikTok';
-                            break;
-                        case platform.Twitter:
-                            url = url.replace(platform.Twitter, altPlatform.TwitterX);
-                            videoType = 'X';
-                            break;
-                        case platform.X:
-                            url = url.replace(platform.X, altPlatform.TwitterX);
-                            videoType = 'X';
-                            break;
-                        default:
-                            break;
-                    }
+					switch (url) {
+						case platform.Instagram:
+							url = url.replace(platform.Instagram, altPlatform.Instagram);
+							videoType = 'Instagram Reel';
+							break;
+						case platform.TikTok:
+							url = url.replace(platform.TikTok, altPlatform.TikTok);
+							videoType = 'TikTok';
+							break;
+						case platform.Twitter:
+							url = url.replace(platform.Twitter, altPlatform.Twitter);
+							videoType = 'Twitter';
+							break;
+						case platform.X:
+							url = url.replace(platform.X, altPlatform.TwitterX);
+							videoType = 'X';
+							break;
+						default:
+							break;
+					}
                     return res.send({
                         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                         data: { content: `[${videoType}](${url}) shared by ${member.user.username}:` },
