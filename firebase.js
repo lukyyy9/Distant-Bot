00892df44f7d59cff6a2062fabf300018bb10e0e
Lucas <65600775+lukyyy9@users.noreply.github.com>
2024-03-26 10:24:10 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,8 +2,8 @@ const admin = require('firebase-admin');
 let serviceAccount = {
     type: process.env.FB_TYPE,
     project_id: process.env.FB_PROJECT_ID,
-    private_key_id: process.env.FB_PRIVATE_KEY_ID ? JSON.parse(process.env.FB_PRIVATE_KEY_ID) : undefined,
-    private_key: process.env.FB_PRIVATE_KEY,
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FB_CLIENT_EMAIL,
     client_id: process.env.FB_CLIENT_ID,
     auth_uri: process.env.FB_AUTH_URI,
