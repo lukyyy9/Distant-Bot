11bf3ad79014dec3914396ea0aa9e934fcf0020c
Lucas <65600775+lukyyy9@users.noreply.github.com>
2024-04-02 15:49:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -161,6 +161,7 @@ app.post('/interactions', verifyMiddleware, async (req, res) => {
 					type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 					data: {
 						content: `Post upvoted by <@${member.user.id}>`,
+                        flags: 64
 					}
 				});
         }
