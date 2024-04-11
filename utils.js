35b6257c67eb7af846e6bc9bbc7b930f6acb67eb
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2024-04-11 15:00:10 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -13,6 +13,7 @@ async function getSpotifyAccessToken(clientId, clientSecret) {
         },
       }
     );
+    console.log('Spotify token refreshed');
     return response.data.access_token;
   } catch (error) {
     console.error('Error fetching Spotify access token:', error);
